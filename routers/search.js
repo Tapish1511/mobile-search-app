f3ebfe28e53a148d17f54b9cdafa42bd10d2c962
Tapish1511 <87308840+Tapish1511@users.noreply.github.com>
2023-01-22 15:18:01 +0530

    added search view to the router

--- routers/search.js
+++ routers/search.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const cheerio = require('cheerio');
+
+const router = express.Router();
+
+
+router.get("/", (req, res)=>{
+    res.render('search');
+})
+
+router.post('/', (req, res)=>{
+
+})
+
+module.exports = router;
