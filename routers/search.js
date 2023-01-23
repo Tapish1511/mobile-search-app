430b9b65d7ad76ca84fe6551fbec0e7769db0d1d
Tapish1511 <87308840+Tapish1511@users.noreply.github.com>
2023-01-23 11:02:32 +0530

    basic data retrevied from amazon

--- routers/search.js
+++ routers/search.js
@@ -1,15 +1,63 @@
 const express = require('express');
+const path = require('path');
+const axios = require('axios');
 const cheerio = require('cheerio');
+const { readdirSync } = require('fs');
 
 const router = express.Router();
+router.use(express.static(path.join(__dirname, 'public')));
 
+const amazonUrl = 'https://www.amazon.in/s?k='
 
-router.get("/", (req, res)=>{
-    res.render('search');
+router.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, '..', 'public', 'search', 'search.html'));
 })
 
 router.post('/', (req, res)=>{
-
+    console.log(req.body.query)
+    let query = req.body.query;
+    getData(query, res);
+    
 })
+
+async function getData(query, res){
+    
+    axios.get(amazonUrl+query)
+    .then((response)=>{
+        const $ = cheerio.load(response.data);
+        
+        const scrapData = [];
+        
+        const row = $('.s-result-item').find('.sg-row');
+        // .each((i, element)=>{
+        //     scrapData.push({
+        //         img: $(ele)
+        //         price: $(element).find('.a-price-whole').text(),
+        //         features: $(element).find('.a-list-item').text()
+        //     });
+
+        // });
+        for(let i=0; i<row.length; i++){
+            scrapData.push({
+                img: $(row[i]).find('.s-image').toString(),
+                price: $(row[i]).find('.a-price-whole').text(),
+                features: $(row[i]).find('.a-size-medium, .a-color-base, .a-text-nromal').text()
+            });
+        }
+       
+        const jsonData = JSON.stringify(scrapData);
+        res.json(scrapData);
+    
+    })
+    .catch(error=>{
+        console.log("error");
+    })
+
+    
+    
+}
+
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,31 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use(express.urlencoded({extended:true}));
 
 
+app.get('/', (req, res)=>{
+    res.render('index', {text:"world"});
+})
+
+// this get function will redirect to the search page.
+
+
+
+const searchRouter = require('./routers/search');
+app.use('/search', searchRouter);
+
+app.post('/', (req, res)=>{
+    isvalid = true;
+    if(isvalid){
+        res.redirect('/search');
+    }
+    else{
+        res.redirect('./');
+    }
+});
 
 app.listen(3000);
